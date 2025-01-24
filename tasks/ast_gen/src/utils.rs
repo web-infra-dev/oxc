139bebecf992454ae7b3bc71cfe4a31109261588
overlookmotel <theoverlookmotel@gmail.com>
2025-01-24 21:04:45 +0000

    Doc comments

--- tasks/ast_gen/src/utils.rs
+++ tasks/ast_gen/src/utils.rs
@@ -1,7 +1,8 @@
 use quote::format_ident;
 use syn::Ident;
 
-// From https://doc.rust-lang.org/reference/keywords.html
+/// Reserved word in Rust.
+/// From https://doc.rust-lang.org/reference/keywords.html
 #[rustfmt::skip]
 static RESERVED_NAMES: &[&str] = &[
     // Strict keywords
@@ -16,10 +17,15 @@ static RESERVED_NAMES: &[&str] = &[
     "macro_rules", "union", // "dyn" also listed as a weak keyword, but is already on strict list
 ];
 
+/// Returns `true` if `name` is a reserved word in Rust.
 pub fn is_reserved_name(name: &str) -> bool {
     RESERVED_NAMES.contains(&name)
 }
 
+/// Create an [`Ident`] from a string.
+///
+/// If the name is a reserved word, it's prepended with `r#`.
+/// e.g. `type` -> `r#type`.
 pub fn create_ident(name: &str) -> Ident {
     if is_reserved_name(name) {
         format_ident!("r#{name}")
